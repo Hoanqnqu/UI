8a4266cb931a818222deb4ef175d057f12890d48
Nhat Hoang <hoanqnqu.dev@gmail.com>
2024-05-12 20:56:04 +0700

    UI

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,28 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ["@babel/plugin-transform-private-methods", { "loose": true }],
+    ['nativewind/babel'],
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
+  ],
 };
---
+++ src/utils/ApiKey.js
@@ -0,0 +1,1 @@
+export const newsApiKey = "bd2a5cf4edd14b918b366d5affb30dcc";
